36f8fe1215d11dfa6951927d8b8d66ccc3814092
ffsedd <ffsedd@gmail.com>
2019-06-24 22:33:04 +0200

    timeit

--- npimage.py
+++ npimage.py
@@ -22,21 +22,14 @@ HISTOGRAM_BINS = 256
 
 def timeit(method):
 
-    import time
-
     def timed(*args, **kw):
         ts = time.time()
         result = method(*args, **kw)
         te = time.time()
-        if 'log_time' in kw:
-            name = kw.get('log_name', method.__name__.upper())
-            kw['log_time'][name] = int((te - ts) * 1000)
-        else:
-            print('%r  %2.2f ms' %
-                  (method.__name__, (te - ts) * 1000))
+        print('%r  %2.2f ms' % (method.__qualname__, (te - ts) * 1000))
         return result
     return timed
-
+    
 
 class npImage():
 
