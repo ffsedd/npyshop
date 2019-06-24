c4d1b9eb75b668d7ca8dc03ea715fabd2c94c693
ffsedd <ffsedd@gmail.com>
2019-06-24 13:59:11 +0200

    removed skimage

--- nputils.py
+++ nputils.py
@@ -6,17 +6,10 @@ import logging
 import numpy as np
 
 from matplotlib import pyplot as plt
-from skimage import img_as_ubyte, img_as_uint
-# img_as_float - data normalized !!!
 
 from pathlib import Path
 
 from imageio import imread, imwrite
-
-
-
-
-
 
 
 def int_to_float(arr):
@@ -27,9 +20,23 @@ def int_to_float(arr):
         y = arr / 65535
     return y.astype(np.float32)
 
+
+def float_to_int(float_arr, bitdepth):
+
+    if bitdepth == 8:
+        arr = 255 * float_arr
+        return arr.astype(np.uint8)
+    elif bitdepth == 16:  # 16bit PNG - default output
+        arr = 65535 * float_arr
+        return arr.astype(np.uint16)
+    else:
+        raise(f"Not implemented bitdepth {bitdepth}")
+
+
 def load_image(fp):
     ''' load image from fp and return numpy uint8 or uint16 '''
     return imread(fp)
+
 
 def get_bitdepth(arr):
 
@@ -42,45 +49,39 @@ def get_bitdepth(arr):
 def normalize(y, inrange=None, outrange=(0, 1)):
     ''' Normalize numpy array --> values 0...1 '''
 
-    imin, imax = inrange if inrange else ( np.min(y), np.max(y) )
+    imin, imax = inrange if inrange else (np.min(y), np.max(y))
     omin, omax = outrange
-    logging.debug(f"normalize array, limits - in: {imin},{imax} out: {omin},{omax}")
+    logging.debug(f"normalize array, limits - in: \
+        {imin}, {imax} out: {omin}, {omax}")
 
-    return np.clip( omin + omax * (y - imin) / (imax - imin), a_min=omin, a_max=omax )
+    return np.clip(omin + omax * (y - imin) / (imax - imin),
+                   a_min=omin, a_max=omax)
 
 
-
-def save_image(im, fp_out, bitdepth=None, mode=None):
-    ''' float or uint16 numpy array --> 16 bit png
-        uint8 numpy array --> 8bit png '''
+def save_image(float_arr, fp_out, bitdepth=8):
+    ''' '''
 
     logging.debug(f"saving image {fp_out}")
+    assert isinstance(float_arr, (np.ndarray, np.generic))
+
     Fp = Path(fp_out)
     Fp.parent.mkdir(exist_ok=True)
 
-    assert isinstance(im, (np.ndarray, np.generic))
-    im = np.clip(im,a_min=0,a_max=1)
+    float_arr = np.clip(float_arr, a_min=0, a_max=1)
 
-    # convert float to int
-    if bitdepth == 8:
-        im = img_as_ubyte(im)
-    elif bitdepth == 16: # 16bit PNG - default output
-        im = img_as_uint(im)  # accept float
-    else:
-        raise Exception(f"You must specify bitdepth:{bitdepth}")
-    imwrite(Fp, im)
+    arr = float_to_int(float_arr, bitdepth=bitdepth)
+
+    imwrite(Fp, arr)
 
     logging.debug(f"image saved")
+
 
 def plti(im, name="", plot_axis=False, vmin=0, vmax=1, **kwargs):
 
     cmap = "gray" if im.ndim == 2 else "jet"
     plt.title(name)
-    plt.imshow(im, interpolation="none", cmap=cmap, vmin=vmin, vmax=vmax, **kwargs)
+    plt.imshow(im, interpolation="none", cmap=cmap, vmin=vmin,
+               vmax=vmax, **kwargs)
     if not plot_axis:
         plt.axis('off')  # turn off axis
     plt.show()
-
-
-
-
